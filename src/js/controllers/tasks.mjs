39979e7fe5bbd547a88000c2f82e4bf9ac4e9e79
KhrisDV <KhrisQ@protonmail.com>
2022-03-09 13:53:15 +0100

    HideShow

--- src/js/controllers/tasks.mjs
+++ src/js/controllers/tasks.mjs
@@ -1,56 +1,76 @@
 import { addTask, saveTasks, getTasks } from "../models/domainObjects.mjs";
-import { taskListHTMLSelector, addTaskInputSelector, completedCSSClass } from "../models/defines.mjs"
+import {
+  taskListHTMLSelector,
+  addTaskInputSelector,
+  completedCSSClass,
+  checkedCSSClass,
+  checkHideShowList,
+} from "../models/defines.mjs";
 
-export function task2HTMLElement (taskIndex, taskObject) {
-    // Creo los elementos HTML
-    const listHTMLItem = document.createElement("li");
-    const pHTMLItem = document.createElement("p");
-    const inputCheckboxHTMLItem = document.createElement("input");
-    // Les proporciono valores 
-    inputCheckboxHTMLItem.type = "checkbox";
-    inputCheckboxHTMLItem.checked = taskObject.completed;
-    pHTMLItem.innerHTML = taskObject.taskName
-    // Los anido
-    listHTMLItem.append(pHTMLItem, inputCheckboxHTMLItem);
-    // Aplico estilos si está completada
-    if (taskObject.completed) {
-        listHTMLItem.classList.add(completedCSSClass);
-    } else {
-        listHTMLItem.classList.remove(completedCSSClass);
-    }
-    // Añado el manejador de eventos
-    inputCheckboxHTMLItem.addEventListener(
-        "click",
-        (event) => {
-            const tasks = getTasks();
-            tasks[taskIndex].completed = event.target.checked;
-            saveTasks(tasks);
-        }
-    );
-    return listHTMLItem
+export function task2HTMLElement(taskIndex, taskObject) {
+  // Creo los elementos HTML
+  const listHTMLItem = document.createElement("li");
+  const pHTMLItem = document.createElement("p");
+  const inputCheckboxHTMLItem = document.createElement("input");
+  // Les proporciono valores
+  inputCheckboxHTMLItem.type = "checkbox";
+  inputCheckboxHTMLItem.checked = taskObject.completed;
+  pHTMLItem.innerHTML = taskObject.taskName;
+  // Los anido
+  listHTMLItem.append(pHTMLItem, inputCheckboxHTMLItem);
+  // Aplico estilos si está completada
+  if (taskObject.completed) {
+    listHTMLItem.classList.add(completedCSSClass);
+  } else {
+    listHTMLItem.classList.remove(completedCSSClass);
+  }
+  // Añado el manejador de eventos
+  inputCheckboxHTMLItem.addEventListener("click", (event) => {
+    const tasks = getTasks();
+    tasks[taskIndex].completed = event.target.checked;
+    saveTasks(tasks);
+  });
+  return listHTMLItem;
 }
 
-export function updateTasksHTML (CSSselector, tasksArray) {
-    const listHTMLElement = document.querySelector(CSSselector);
-    listHTMLElement.innerText = ""
-    if (tasksArray.length > 0) {
-        for ( let index in tasksArray ) {
-            listHTMLElement.appendChild(task2HTMLElement(index, tasksArray[index]))
-        }
-    } else {
-        listHTMLElement.innerText = "Add your first task..."
+export function updateTasksHTML(CSSselector, tasksArray) {
+  const listHTMLElement = document.querySelector(CSSselector);
+  listHTMLElement.innerText = "";
+  if (tasksArray.length > 0) {
+    for (let index in tasksArray) {
+      listHTMLElement.appendChild(task2HTMLElement(index, tasksArray[index]));
     }
-
+  } else {
+    listHTMLElement.innerText = "Add your first task...";
+  }
 }
 
-export function taskAddButtonClickHandler (event) {
-    //console.log(event)
-    const input = document.querySelector(addTaskInputSelector);
-    event.preventDefault()
-    const newTask = {
-        taskName: input.value,
-        completed: false,
-    };
-    addTask(newTask);
-    updateTasksHTML(taskListHTMLSelector,getTasks());
+export function taskAddButtonClickHandler(event) {
+  const input = document.querySelector(addTaskInputSelector);
+  event.preventDefault();
+  const newTask = {
+    taskName: input.value,
+    completed: false,
+  };
+  addTask(newTask);
+  updateTasksHTML(taskListHTMLSelector, getTasks());
+}
+
+hideShow.addEventListener("click", (event) => {
+  checkclickhandler(hideShow);
+});
+
+function checkclickhandler(hideShow) {
+  const listaOcultar = document.querySelector(taskListHTMLSelector);
+  const tareahecha = listaOcultar.querySelectorAll(".completed");
+  console.log(tareahecha);
+  console.log(listaOcultar.completed);
+
+  /// TODO
+  if (hideShow.checked) {
+      }
+    tareahecha.classList.add(checkedCSSClass);
+  } else {
+    listaOcultar.classList.remove(checkedCSSClass);
+  }
 }
--- src/js/models/defines.mjs
+++ src/js/models/defines.mjs
@@ -2,3 +2,5 @@ export const taskListHTMLSelector = "#tasksList";
 export const addTaskButtonSelector = "#addTaskButton";
 export const addTaskInputSelector = "#taskInput";
 export const completedCSSClass = "completed";
+export const checkHideShowList = "#hideShow";
+export const checkedCSSClass = "checked";
